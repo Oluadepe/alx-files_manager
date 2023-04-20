52188326195cbb58587ce63d00c18c3dfe19cc7f
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-20 15:25:43 +0100

    applied a fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 
-
 const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
